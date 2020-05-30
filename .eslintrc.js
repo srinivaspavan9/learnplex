4d8a8e7323153b02224a5c5043deb84604d77a60
Bhanu Teja P <pbteja1998@gmail.com>
2020-05-30 16:48:31 +0530

    Use dokz.site

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ['prettier'],
-  extends: ['react-app', 'plugin:prettier/recommended'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-  },
-}
---
+++ pages/_app.jsx
@@ -0,0 +1,50 @@
+import { DokzProvider, GithubLink, ColorModeSwitch } from 'dokz/dist'
+import React from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+export default (props) => {
+  const { Component, pageProps } = props
+  const router = useRouter()
+  const path = router.asPath
+  let titleInLowercase = path.slice(1, path.length)
+  if (titleInLowercase === '') {
+    titleInLowercase = 'about'
+  }
+  const title =
+    titleInLowercase[0].toUpperCase() +
+    titleInLowercase.slice(1, titleInLowercase.length) +
+    ' - Learnplex Docs'
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <DokzProvider
+        headerLogo={
+          <>
+            <img
+              src="/logo.png"
+              alt=""
+              style={{ width: '50px', height: '50px' }}
+            />
+            &nbsp;Learnplex Docs
+          </>
+        }
+        headerItems={[
+          <GithubLink key="0" url="https://github.com/remorses/dokz" />,
+          <ColorModeSwitch key="1" />,
+        ]}
+        sidebarOrdering={{
+          'index.mdx': false,
+          'about.mdx': true,
+          'prerequisites.mdx': true,
+          'installation.mdx': true,
+          'contributing.mdx': true,
+        }}
+      >
+        <Component {...pageProps} />
+      </DokzProvider>
+    </>
+  )
+}
